3618512742b45fba7214fcde20c8d5c8e47f60fe
Brooks Benson <brooksbenson03@gmail.com>
2021-03-14 21:14:26 -0600

    refactor: previewer, bundler

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,86 @@
+import { useRef } from 'react'
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
+import codeShift from 'jscodeshift'
+import Highlighter from 'monaco-jsx-highlighter'
+import './code-editor.css'
+import './syntax.css'
+
+interface CodeEditorProps {
+  handleChange: (mutation: string) => void
+  initialValue: string
+}
+
+export const CodeEditor: React.FC<CodeEditorProps> = ({
+  handleChange,
+  initialValue
+}) => {
+  const editorRef = useRef<any>()
+
+  const onEditorDidMount: EditorDidMount = (getValue, editor) => {
+    editorRef.current = editor
+
+    editor.onDidChangeModelContent(() => {
+      handleChange(getValue())
+    })
+
+    editor.getModel()?.updateOptions({ tabSize: 2 })
+
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeShift,
+      editor
+    )
+
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    )
+  }
+
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue()
+    const formatted = prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: false,
+        singleQuote: true
+      })
+      .replace(/\n$/, '')
+    editorRef.current.setValue(formatted)
+  }
+
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary button-is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        height="500px"
+        language="javascript"
+        theme="dark"
+        options={{
+          automaticLayout: true,
+          folding: false,
+          fontSize: 16,
+          lineNumbersMinChars: 3,
+          minimap: { enabled: false },
+          scrollBeyondLastLine: false,
+          showUnused: false,
+          wordWrap: 'on'
+        }}
+        value={initialValue}
+      />
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,78 +1,27 @@
-import * as esbuild from 'esbuild-wasm'
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
-import { useState, useEffect, useRef } from 'react'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
-import { fetchPlugin } from './plugins/fetch-plugin'
+import { useState } from 'react'
+import { CodeEditor } from './components/code-editor'
+import Preview from './components/preview'
+import bundle from './bundler'
 
 const App = () => {
-  const builder = useRef<any>()
-  const iframe = useRef<any>()
   const [input, setInput] = useState<any>()
-
-  const createBuilder = async () => {
-    builder.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    })
-  }
-
-  useEffect(() => {
-    createBuilder()
-  }, [])
+  const [code, setCode] = useState('')
 
   const onClick = async () => {
-    if (!builder.current) return
-
-    iframe.current.srcDoc = html
-
-    const res = await builder.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
-
-    iframe.current.contentWindow.postMessage(res.outputFiles[0].text, '*')
+    const output = await bundle(input)
+    setCode(output)
   }
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          const executeScript = event => {
-            try {
-              eval(event.data)
-            } catch (e) {
-              const root = document.querySelector('#root')
-              root.innerHTML = '<div>' + e + '</div>'
-            }
-          }
-          window.addEventListener('message', executeScript)
-        </script>
-      </body>
-    </html>
-  `
 
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <div />
+      <CodeEditor
+        initialValue="const a = 'a'"
+        handleChange={(change) => setInput(change)}
+      />
       <button onClick={onClick}>Submit</button>
-      <iframe
-        title="preview"
-        ref={iframe}
-        srcDoc={html}
-        sandbox="allow-scripts"
-      ></iframe>
+      <Preview code={code} />
     </div>
   )
 }
